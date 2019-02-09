be1959342af39d384c9d2a7ff4d288dcf6d81974
OE10x2 <45720669+OE10x2@users.noreply.github.com>
2019-02-09 15:13:46 -0500

    Update cco16p4.cpp

--- cco16p4.cpp
+++ cco16p4.cpp
@@ -3,14 +3,12 @@
 using namespace std;
 
 unordered_map<string, int> check;
-set<string> pos;
 bool flip[MM][MM];
 char line[MM];
 
 int main(){
   int N, G; scanf("%d%d", &N, &G);
   for (int i = 0; i < G; i++){
-    memset(flip, 0, sizeof(flip));
     for (int j = 1; j <= N; j++){
       scanf("%s", line);
       for (int k = 1; k <= N; k++) flip[j][k] = line[k-1] == 'R';
@@ -29,10 +27,9 @@ int main(){
     for (int j = 1; j <= N; j++) temp += flip[N][j] == 1 ? 'R' : 'W';
     for (int j = N-1; j >= 1; j--) temp += flip[j][N] == 1 ? 'R' : 'W';
     check[temp]++;
-    pos.insert(temp);
   }
   int ans = 0;
-  for (string i: pos) ans += check[i] * (check[i]-1) / 2;
+  for (auto i: check) ans += i.second * (i.second - 1) / 2;
   printf("%d\n", ans);
   return 0;
 }
